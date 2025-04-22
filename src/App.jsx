3f40aa9cafb6d736e8bf6ff6a124e63c68ff513f
webster3141 <53284446+webster3141@users.noreply.github.com>
2025-04-22 08:58:01 -0500

    Initial commit of PDFQuickCopy app

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import PDFUploader from './components/PDFUploader';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white text-gray-800">
+      <PDFUploader />
+    </div>
+  );
+}
+
+export default App;
